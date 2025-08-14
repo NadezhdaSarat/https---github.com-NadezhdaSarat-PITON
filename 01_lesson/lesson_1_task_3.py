dea6bc5ba08758fcb2c6ebb55950ff16e4ed61c8
NadezhdaS78 <saranadezhda1978@yandex.ru>
2025-08-14 16:46:03 +0300

    1.1

---
+++ 01_lesson/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input("Nadezhda")
+last_name = input("Saratovceva")
+print(f"Вас зовут: {last_name} {first_name}")
--- list.py
+++ list.py
@@ -1,3 +1,25 @@
 my_heigh = 168
 print(my_heigh)
 
+my_name = "Nadezhda"
+my_name = "Nadezhda Saratovceva"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+input = "Эльза"
+print( "Ваш любимчик - " + pet_name)
+
+def print_python() :
+    print("Учу Python!")
+print_python()  
+
+def print_letter(let):
+    print(let, end='')
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
+
