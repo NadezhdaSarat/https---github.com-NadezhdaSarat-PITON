b053527ff75b5cb584272fb485543298681578b0
NadezhdaS78 <saranadezhda1978@yandex.ru>
2025-08-18 07:27:24 +0300

    2

--- 01_lesson/lesson_1_task_3.py
+++ 01_lesson/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-first_name = input("Nadezhda")
-last_name = input("Saratovceva")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 print(f"Вас зовут: {last_name} {first_name}")
